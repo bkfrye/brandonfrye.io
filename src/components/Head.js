0c8b1a6825913f2945615541e42827571e50c8c7
Brandon Frye <brandonkfrye@gmail.com>
2018-01-19 18:52:49 -0500

    added NewRelic

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import Config from './config'
+import NewRelic from 'new-relic-agent-react'
 import { Helmet } from 'react-helmet'
 import ReactGA from 'react-ga'
 import screenshot from '../brandonfrye.jpg'
@@ -10,19 +12,22 @@ class Head extends Component {
 	}
 	render() {
 		return (
-			<Helmet>
-				<title itemProp="name" lang="en">Brandon Frye - Front-end Developer and Designer</title>
-				<link rel="canonical" href={window.location.href} />
-				<meta property="og:type" content="website" />
-				<meta property="og:title" content="Brandon Frye - Front-end Developer and Designer" />
-				<meta property="twitter:title" content="Brandon Frye - Front-end Developer and Designer" />
-				<meta property="og:url" content={window.location.href} />
-				<meta name="description" content="Brandon Frye is developer and designer with a passion for UX. Building beautiful websites is the core of what I love to do."/>
-				<meta name="og:description" content="Brandon Frye is developer and designer with a passion for UX. Building beautiful websites is the core of what I love to do."/>
-				<meta name="twitter:description" content="Brandon Frye is developer and designer with a passion for UX. Building beautiful websites is the core of what I love to do."/>
-				<meta property="og:image" content={screenshot} />
-				<meta property="twitter:image" content={screenshot} />
-			</Helmet>
+			<div>
+				<Helmet>
+					<title itemProp="name" lang="en">Brandon Frye - Front-end Developer and Designer</title>
+					<link rel="canonical" href={window.location.href} />
+					<meta property="og:type" content="website" />
+					<meta property="og:title" content="Brandon Frye - Front-end Developer and Designer" />
+					<meta property="twitter:title" content="Brandon Frye - Front-end Developer and Designer" />
+					<meta property="og:url" content={window.location.href} />
+					<meta name="description" content="Brandon Frye is developer and designer with a passion for UX. Building beautiful websites is the core of what I love to do."/>
+					<meta name="og:description" content="Brandon Frye is developer and designer with a passion for UX. Building beautiful websites is the core of what I love to do."/>
+					<meta name="twitter:description" content="Brandon Frye is developer and designer with a passion for UX. Building beautiful websites is the core of what I love to do."/>
+					<meta property="og:image" content={screenshot} />
+					<meta property="twitter:image" content={screenshot} />
+				</Helmet>
+				<NewRelic licenseKey={ Config.API_KEY } applicationID={ Config.APP_NAME } />
+			</div>
 		)
 	}
 }
