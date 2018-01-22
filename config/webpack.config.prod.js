4dd2fcce1aef5ed0f866b3e787b63c3fa2a9fe97
Brandon Frye <brandonkfrye@gmail.com>
2018-01-22 11:43:19 -0500

    updated webpack build

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,4 +1,3 @@
-// const autoprefixer = require('autoprefixer');
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -52,7 +51,10 @@ module.exports = {
   // You can exclude the *.map files from the build during deployment.
   devtool: 'source-map',
   // In production, we only want to load the polyfills and the app code.
-  entry: [require.resolve('./polyfills'), paths.appIndexJs],
+  entry: [
+      require.resolve('./polyfills'),
+      paths.appIndexJs
+  ],
   output: {
     // The build folder.
     path: paths.appBuild,
