f4ac0dd91d8c67c1261038cec731f77e849c89f2
Brandon Frye <brandonkfrye@gmail.com>
2018-07-18 09:38:51 -0400

    removed api data from component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,9 @@
 import React, {Component} from 'react'
 import GSAP from 'react-gsap-enhancer'
-import DataAPI from '../api.js'
 import Intro from './Intro'
 import SSH from './SSH'
 
 class Home extends Component {
-
-	constructor() {
-		super()
-		let data = DataAPI.content;
-
-		this.state = {
-			sshKey: data.ssh.key
-		}
-	}
 
 	render() {
 		return (
