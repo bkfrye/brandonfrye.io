8d48db7ad1ad60a763e3712d2b6957bf8f674341
Brandon Frye <brandonkfrye@gmail.com>
2018-07-18 09:30:19 -0400

    updated new tabs

--- src/api.js
+++ src/api.js
@@ -1,13 +1,13 @@
 const DataAPI = {
-    content: {
-        intro: {
-            title: "brandon frye.",
-            copy: "creative front-end development"
-        },
-        ssh: {
-            key: "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5rmloiWWxAW/ENh9XhKzGavg9ht46Pv6zg5FPydZiMoomqSMsVUeSw2Mwko5gT4DVflAtBxB0eXG1sWTDrHwBhyPD7WDaQrp1BMOGENA/ntBRXZlkNeaOndDIJi7NxqlHTUFt04W35+zJ/9V16bgZxnm+rv8PTV84c3GIDT9JShlxUopmUldVdp5c8JwjeYWAjuP/ebn1rsN6sFPeiS47RGlJhaC5mFDSwdJPwgmwP99Qkd+/Y8QVVhtGMAydQAeAm9tBGhnvRTE0EdWx2kJX/hC9K1klfWhZw6ysIouuo7ehcVTKQ+oqr1MCEGIfcmAXtfNYMLtSHMslYDhlNSud"
-        }
+  content: {
+    intro: {
+      title: "brandon frye.",
+      copy: "creative front-end development"
     },
+    ssh: {
+      key: "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5rmloiWWxAW/ENh9XhKzGavg9ht46Pv6zg5FPydZiMoomqSMsVUeSw2Mwko5gT4DVflAtBxB0eXG1sWTDrHwBhyPD7WDaQrp1BMOGENA/ntBRXZlkNeaOndDIJi7NxqlHTUFt04W35+zJ/9V16bgZxnm+rv8PTV84c3GIDT9JShlxUopmUldVdp5c8JwjeYWAjuP/ebn1rsN6sFPeiS47RGlJhaC5mFDSwdJPwgmwP99Qkd+/Y8QVVhtGMAydQAeAm9tBGhnvRTE0EdWx2kJX/hC9K1klfWhZw6ysIouuo7ehcVTKQ+oqr1MCEGIfcmAXtfNYMLtSHMslYDhlNSud"
+    }
+  },
 }
 
 export default DataAPI
