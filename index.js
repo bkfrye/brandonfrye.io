2ac34cf6b5b726523633bcf05160b734aab91a0c
Brandon Frye <brandonkfrye@gmail.com>
2018-11-08 13:52:42 -0500

    update to express backend

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const robots = require('express-robots')
+
+app.use(require('prerender-node').set('prerenderToken', '2J9WnuZJZYnaCOL4flB0'))
+app.use(express.static(path.join(__dirname, 'client/build')))
+app.use(robots({ UserAgent: '*', Disallow: '' }))
+
+
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+})
+
+app.get('*', function(req, res) {
+	res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+})
+
+// app.get('/resume', function(reg, res) {
+// 	res.sendFile(path.join(__dirname, 'client/build', 'resume.html'));
+// })
+
+app.listen(process.env.PORT || 5000)
+
+console.log('Listening on PORT:5000')
